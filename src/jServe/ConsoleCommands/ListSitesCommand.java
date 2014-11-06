e91b2192937c4b8c2c1bea2cefbe805e441e3d7c
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-11-05 23:10:48 -0500

    Some style edits to ListSitesCommand

--- src/jServe/ConsoleCommands/ListSitesCommand.java
+++ src/jServe/ConsoleCommands/ListSitesCommand.java
@@ -1,26 +1,31 @@
 package jServe.ConsoleCommands;
 
-import java.io.PrintWriter;
-
-import jServe.Core.*;
+import jServe.Core.CommandLine;
+import jServe.Core.Utils;
+import jServe.Core.WebServer;
 import jServe.Sites.Site;
 
-public class ListSitesCommand extends CLICommand {    
+public class ListSitesCommand extends CLICommand {
     static {
         register("list", new ListSitesCommand());
     }
-    
+
     /**
-     * Hook function for whenever the user calls this command through the CommandLine
-     * @param arg0 Normally a String
+     * Hook function for whenever the user calls this command through the
+     * CommandLine
+     * 
+     * @param arg0
+     *            Normally a String
      */
+    @Override
     public void run(Object arg0) {
-        
-        String[] args = ((String)arg0).split(" ");
-            
+
+        String[] args = ((String) arg0).split(" ");
+
         CommandLine c = WebServer.COMMAND_LINE;
         for (Site s : WebServer.sites) {
-            c.println(Utils.padRight("" + s.getID(), 5) + " | " + Utils.padTruncateRight(s.getName(), 15) + " | " + s.getStatus().toString() );
+            c.println(Utils.padRight("" + s.getID(), 5) + " | " + Utils.padTruncateRight(s.getName(), 15) + " | "
+                      + s.getStatus().toString());
         }
         c.println("Total of " + WebServer.sites.size() + " Sites Registered");
     }
