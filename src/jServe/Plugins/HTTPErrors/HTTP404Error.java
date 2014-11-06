9f91f72488f44c89a914a8585a40f5408a5091f4
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-11-05 23:05:23 -0500

    Updated HTTP404Error to extend the new HTTPError Class

--- src/jServe/Plugins/HTTPErrors/HTTP404Error.java
+++ src/jServe/Plugins/HTTPErrors/HTTP404Error.java
@@ -1,9 +1,26 @@
 package jServe.Plugins.HTTPErrors;
 
-public class HTTP404Error {
+/**
+ * An Exception for when the server encounters a 404 error. An instance of this
+ * class will get attached to the request that raised the exception and server
+ * can then use the information provided by this class to respond correctly to
+ * the Client and report the problem
+ * 
+ * @author Edward Hurtig <hurtige@ccs.neu.edu>
+ * @version Oct 16, 2014
+ */
+public class HTTP404Error extends HTTPError {
 
+    /**
+     * Serial Version
+     */
+    private static final long serialVersionUID = 2823807786897121270L;
+
+    /**
+     * Constructs an HTTP 404 Error Object to be used with a given request
+     */
     public HTTP404Error() {
-
+        super(404.0, "Not Found");
     }
 
 }
