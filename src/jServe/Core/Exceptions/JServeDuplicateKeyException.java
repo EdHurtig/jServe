d9154721762667d1819b1c878058bb255b1b467a
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-11-05 23:04:34 -0500

    Started new package for jServe specific Exceptions

---
+++ src/jServe/Core/Exceptions/JServeDuplicateKeyException.java
@@ -0,0 +1,19 @@
+package jServe.Core.Exceptions;
+
+/**
+ * Represents an Exception that is thrown when some function encounters a key
+ * that already exists in some key-value structure and that key cannot be
+ * overwritten or the function has not been informed to that overwriting is
+ * allowed if it exposes that functionality
+ *
+ * @author Eddie Hurtig <hurtige@ccs.neu.edu>
+ * @version 1.0, 11/05/2014
+ */
+public class JServeDuplicateKeyException extends RuntimeException {
+    public JServeDuplicateKeyException() {
+        super("A Duplicate key was found");
+    }
+    public JServeDuplicateKeyException(Object key) {
+        super("A Duplicate key '" + key.toString() + "' was found");
+    }
+}
