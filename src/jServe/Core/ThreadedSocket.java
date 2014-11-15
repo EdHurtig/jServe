d1dd14738d6ba8990c3d8ac0c7d2c165591cc226
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-11-14 21:42:18 -0500

    Clean up ThreadedSocket and added docs

--- src/jServe/Core/ThreadedSocket.java
+++ src/jServe/Core/ThreadedSocket.java
@@ -1,21 +1,44 @@
 package jServe.Core;
 
-import java.io.BufferedReader;
+import jServe.Core.Exceptions.JServeException;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
-
+/**
+ * An Extension of ServerSocket that will run in a new thread so that it
+ * is non blocking when waiting for a connection.
+ * <p/>
+ * A ThreadedSocket extends Runnable so you can launch it with the Thread class
+ * for asynchronous operation.
+ * <p/>
+ * Every time a new connection is received, a ThreadedSocket will immediately push
+ * that connection into a Request object which runs in yet another thread (spawned by Thread).
+ * The Request object reads the data from the incoming connection and parses it into a type of
+ * Request, currently only HTTP Requests are handled, but theoretically any type of request
+ * could be handled.
+ * <p/>
+ * The Request is then Passed to a Site which processes the Request and using the Request Object's
+ * output stream to send back data to the user
+ */
 public class ThreadedSocket extends ServerSocket implements Runnable {
 
 
+    /**
+     * Constructor for the ThreadedSocket, Takes a port number to listen on
+     *
+     * @param port The port number to listen on
+     * @throws IOException
+     */
     public ThreadedSocket(int port) throws IOException {
         super(port);
     }
 
+    /**
+     * The main Listener Loop Function. Blocks on the ServerSocket's accept() call
+     */
     @Override
     public void run() {
         int n = 0;
@@ -34,12 +57,16 @@ public class ThreadedSocket extends ServerSocket implements Runnable {
         }
         WebServer.logInfo("Thread Registered for port " + getLocalPort());
         while (WebServer.getStatus() == ServerStatus.Started || WebServer.getStatus() == ServerStatus.Starting) {
-            Socket client = null;
 
             // If the socket was closed (no more sites using it), then stop the thread
             if (isClosed()) {
                 return;
             }
+
+            // The Socket Object that represents the raw connection of the next connection
+            Socket client;
+
+            // Block until a new connection arrives
             try {
                 client = accept();
             } catch (IOException e) {
@@ -47,9 +74,10 @@ public class ThreadedSocket extends ServerSocket implements Runnable {
                 continue;
             }
 
-            long start = System.nanoTime();
+            // Create a Request object for this connection
             Request req = new Request(client);
-            //Create thread for the socket
+
+            //Create thread for the request
             Thread requestThread = new Thread(req);
 
             WebServer.logInfo("Starting New Thread For Request " + req.getRequestID());
@@ -58,6 +86,8 @@ public class ThreadedSocket extends ServerSocket implements Runnable {
             requestThread.start();
 
         }
+
+        // Exiting the thread
         WebServer.logInfo("Thread Exit for port " + getLocalPort() + " Server Status = " + WebServer.getStatus());
 
     }
