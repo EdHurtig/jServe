dd48859d0aee49ca9cea649a9f81adb116ae6075
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-11-14 21:45:29 -0500

    Starting to move away from my own custom made utility functions

--- src/jServe/Core/Utils.java
+++ src/jServe/Core/Utils.java
@@ -1,5 +1,7 @@
 package jServe.Core;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.Console;
@@ -222,270 +224,6 @@ public class Utils {
         return line;
     }
 
-    private static String repeat(char c, Integer i) {
-        if (i <= 0) {
-            return "";
-        }
-        return c + repeat(c, --i);
-    }
-
-    private static String repeat(String c, Integer i) {
-        if (i <= 0) {
-            return "";
-        }
-        return c + repeat(c, --i);
-    }
-
-    public static String truncate(String s, int len) {
-        return s.substring(0, len);
-    }
-
-    public static String consoleTable(String[][] data, String[] headers) {
-        // int[] maxlengths = new int[headers.length];
-        //
-        // boolean break2 = false;
-        // for (String[] row : data) {
-        // for (String cell : row) {
-        // if (cell.length() > maxline && cell.length() < maxlength) {
-        // maxline = cell.length();
-        // } else if (cell.length() >= maxlength) {
-        // maxline = maxlength;
-        // break2 = true;
-        // break;
-        // }
-        //
-        // }
-        // if (break2)
-        // break;
-        // }
-
-        String leftAlignFormat = "| %-15s | %-4d |%n";
-
-        // int ncols = headers.length;
-        // int nrows = data.length;
-        //
-        // int width = maxline * ncols + ncols + 2;
-
-        // String table = "";
-        // table = "+" + repeat("-", width - 2) + "+";
-        // table = "|";
-        // for (String h : headers)
-        // table =
-        return "";
-    }
-
-    public static String join(String delimiter, Object[] arr) {
-        String str = "";
-        for (int i = 0; i < arr.length - 1; i++) {
-            str += arr[i] + delimiter;
-        }
-        str += arr[arr.length - 1];
-        return str;
-    }
-
-    public static String join(String delimiter, ArrayList<String> arr) {
-        String str = "";
-        for (String s : arr) {
-            str += s + delimiter;
-        }
-
-        return str.substring(0, -delimiter.length());
-    }
-
-    public static String join(char delimiter, ArrayList<String> arr) {
-        String str = "";
-        for (String s : arr) {
-            str += s + delimiter;
-        }
-
-        return str.substring(0, -1);
-    }
-
-    public static String surroundLR(String s, String lr) {
-        String str = "";
-        String[] lines = s.split("\n");
-        for (String l : lines) {
-            str += lr + s + lr + "\n";
-        }
-
-        return str;
-    }
-
-    public static String surroundLR(String s, char lr) {
-        String str = "";
-        String[] lines = s.split("\n");
-        for (String l : lines) {
-            str += lr + s + lr + "\n";
-        }
-
-        return str;
-    }
-
-    public static String surroundLRCentered(String s, char lr) {
-        return surroundLRCentered(s, lr, longestLine(s.split("\n")) + 2);
-    }
-
-    public static String surroundLRCentered(String s, char lr, int width) {
-        String str = "";
-
-        String[] lines = s.split("\n");
-        for (String l : lines) {
-            str += lr + pad(l, width) + lr + "\n";
-        }
-
-        return str;
-    }
-
-    /**
-     * Returns the length of the longest String in the given String Array
-     *
-     * @param lines The Array to look through
-     * @return The Length of the longest line in the array
-     */
-    public static int longestLine(String[] lines) {
-        int max = 0;
-
-        for (String l : lines) {
-            if (l.length() > max) {
-                max = l.length();
-            }
-        }
-
-        return max;
-    }
-
-    public static String surroundAll(String s, char c) {
-        String str;
-        String[] lines = s.split("\n");
-
-        int longest = longestLine(lines);
-
-        String tb = repeat(c, longest);
-        str = tb;
-
-        for (String l : s.split("\n")) {
-            str += c + l + c;
-        }
-
-        return "Not Implemented";
-    }
-
-    public static String surroundAll(String s, char c, int padding) {
-
-        String[] lines = s.split("\n");
-
-        int longest = longestLine(lines);
-
-        String tb = repeat(c, longest + 2);
-
-        String ws = c + repeat(" ", s.length() - 2) + c + "\n";
-
-        String tbp = repeat(ws, padding);
-
-        String body = surroundLRCentered(s, ' ');
-
-        return "Not Implemented";
-    }
-
-    public static String padTruncateRight(String s, int len) {
-        return padRight(s, len, " ");
-    }
-
-    public static String padTruncateRight(String s, int len, String padstr) {
-        if (s.length() > len) {
-            return truncate(s, len);
-        }
-
-        return padRight(s, len, padstr);
-    }
-
-    public static String padTruncateLeft(String s, int len) {
-        return padLeft(s, len, " ");
-    }
-
-    public static String padTruncateLeft(String s, int len, String padstr) {
-        if (s.length() > len) {
-            return truncate(s, len);
-        }
-
-        return padLeft(s, len, padstr);
-    }
-
-    public static String padTruncate(String s, int len) {
-        return pad(s, len, " ");
-    }
-
-    public static String padTruncate(String s, int len, String padstr) {
-        if (s.length() > len) {
-            return truncate(s, len);
-        }
-
-        return pad(s, len, padstr);
-    }
-
-    public static String padRight(String s, int len) {
-        return padRight(s, len, " ");
-    }
-
-    public static String padRight(String s, int len, String padstr) {
-        if (s.length() >= len) {
-            return s;
-        }
-
-        int nPad = len - s.length();
-
-        String pad = repeat(padstr, nPad / padstr.length());
-        s = s + pad;
-
-        if (s.length() < len) {
-            s += padstr.substring(0, len - s.length());
-        }
-
-        return s;
-    }
-
-    public static String padLeft(String s, int len) {
-        return padLeft(s, len, " ");
-    }
-
-    public static String padLeft(String s, int len, String padstr) {
-        if (s.length() > len) {
-            return s;
-        }
-
-        int nPad = len - s.length();
-
-        String pad = repeat(padstr, nPad / padstr.length());
-        s = s + pad;
-
-        if (s.length() < len) {
-            s += padstr.substring(0, s.length() - len);
-        }
-
-        return s;
-    }
-
-    public static String pad(String s, int len) {
-        return pad(s, len, " ");
-    }
-
-    public static String pad(String s, int len, String padstr) {
-        if (s.length() > len) {
-            return s;
-        }
-
-        int nPad = len - s.length();
-
-        String pad = repeat(padstr, nPad / 2 / padstr.length());
-        s = pad + s + pad;
-
-        if (s.length() < len) {
-            s = padstr.substring(0, len - s.length()) + s;
-        }
-
-        return s;
-    }
-
     public static boolean classExists(String className) {
         try {
             Class.forName(className);
@@ -508,4 +246,34 @@ public class Utils {
         }
         return path;
     }
+
+
+
+
+    /***************************
+     *         Strings         *
+     ***************************/
+
+    public static String pad(String s, int len) {
+        return pad(s, len, " ");
+    }
+
+    public static String pad(String s, int len, String padstr) {
+        if (s.length() > len) {
+            return s;
+        }
+
+        int nPad = len - s.length();
+
+        String pad = StringUtils.repeat(padstr, nPad / 2 / padstr.length());
+        s = pad + s + pad;
+
+        if (s.length() < len) {
+            s = padstr.substring(0, len - s.length()) + s;
+        }
+
+        return s;
+    }
+
+
 }
