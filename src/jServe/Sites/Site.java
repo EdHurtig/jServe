4ab406ac247df5b51ec1c66e94fb3604f3eeedaa
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-11-15 23:51:46 -0500

    Documented and cleaned up... Oh man the bad practices I used to have

--- src/jServe/Sites/Site.java
+++ src/jServe/Sites/Site.java
@@ -8,44 +8,97 @@ import jServe.Core.Plugins;
 import jServe.Core.Request;
 import jServe.Core.ServerStatus;
 import jServe.Core.WebServer;
+import jServe.Plugins.HTTPErrors.HTTPError;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Represents a Site that the Server can route Requests to for
+ * processing.  It provides a standard framework for every site
+ * to interface with for things like:
+ * - Unique Site Identification using getID() and human
+ * identification with getName().
+ * - Centralized Settings Hash
+ * - HTTPError Handling and Responses
+ * - Binding Management
+ */
 public abstract class Site implements Runnable, Configurable {
+
+    /**
+     * The ID of the site
+     */
+    private int ID;
+
+    /**
+     * The Human Recognizable name of the site. Has nothing to do with functionality
+     */
+    private String name;
+
+    /**
+     * The current status of the site
+     */
+    private ServerStatus status = ServerStatus.Stopped;
+
+    /**
+     * The Site's Centralized Settings Hash
+     */
+    private HashMap<String, String> settings = new HashMap<String, String>();
+
+    /**
+     * The Set of Bindings that this Site requires
+     */
+    private ArrayList<Binding> bindings = new ArrayList<Binding>();
+
+    /**
+     * @deprecated
+     */
+    private ArrayList<String> defaultDocuments = new ArrayList<String>();
+
+    /**
+     * Empty Constructor
+     */
     public Site() {
     }
 
+    /**
+     * Initializes a site with an ID and a name
+     *
+     * @param id   The site's ID
+     * @param name The site's Name
+     */
     public Site(int id, String name) {
         setName(name);
         ID = id;
     }
 
     /**
-     * PROPERTIES *
+     * Gets the ID of the site.  IDs are unique integers for each site
+     *
+     * @return The ID of the site
      */
-    int ID;
-    String name;
-    ServerStatus status = ServerStatus.Stopped;
-    HashMap<String, String> settings = new HashMap<String, String>();
-    ArrayList<Binding> bindings = new ArrayList<Binding>();
-
-    ArrayList<String> defaultDocuments = new ArrayList<String>();
-    Request currentRequest;
-
-    /**
-     * GETTERS/SETTERS *
-     */
-
     public int getID() {
         return ID;
     }
 
+    /**
+     * Gets the name of the site.  The name is for human recognition, it has no functional
+     * purpose at this time.
+     *
+     * @return the name of the site
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Updates the name of the site
+     *
+     * @param name Thw new name of the site
+     * @return Success if it was changed, false if it failed to write the config
+     */
     public boolean setName(String name) {
+        //TODO: This ConfigurationManager call needs some reworking
         if (ConfigurationManager.add("sites." + getID() + ".name", this.getName())) {
             this.name = name;
             return true;
@@ -53,11 +106,23 @@ public abstract class Site implements Runnable, Configurable {
         return false;
     }
 
+    /**
+     * Gets the bindings for the site
+     *
+     * @return The current bindings for this site
+     */
     public ArrayList<Binding> getBindings() {
         return bindings;
     }
 
+    /**
+     * Sets the Bindings for the site
+     *
+     * @param bindings The New Bindings
+     * @return Success if it was changed, false if it failed to write the config
+     */
     public boolean setBindings(ArrayList<Binding> bindings) {
+        //TODO: ConfigurationManager
         if (ConfigurationManager.addBindings(bindings, this)) {
             this.bindings = bindings;
             return true;
@@ -65,22 +130,45 @@ public abstract class Site implements Runnable, Configurable {
         return false;
     }
 
+    /**
+     * Returns the centralized Settings Hash of key-value pairs for this site
+     *
+     * @return the Settings Hash
+     */
     public HashMap<String, String> getSettings() {
         return settings;
     }
 
+    /**
+     * Updates the Settings Hash
+     *
+     * @param settings The new Hash
+     */
     public void setSettings(HashMap<String, String> settings) {
         this.settings = settings;
     }
 
+    /**
+     * Gets the Status of this site (starting, started, stopping, stopped, error)
+     *
+     * @return The ServerStatus of this site
+     */
     public ServerStatus getStatus() {
         return status;
     }
 
+    /**
+     * Sets the current status of the site
+     *
+     * @param status The Status of the site
+     */
     public void setStatus(ServerStatus status) {
         this.status = status;
     }
 
+    /**
+     * @deprecated
+     */
     public ArrayList<String> getDefaultDocuments() {
         return defaultDocuments;
     }
@@ -92,8 +180,6 @@ public abstract class Site implements Runnable, Configurable {
 
         this.defaultDocuments = defaultDocuments;
     }
-
-    /** METHODS **/
 
     /**
      * Runs on site startup and thread creation
@@ -114,7 +200,7 @@ public abstract class Site implements Runnable, Configurable {
     /**
      * Runs the site with the given Request
      *
-     * @param r
+     * @param r The Request
      */
     public abstract void run(Request r);
 
@@ -125,66 +211,90 @@ public abstract class Site implements Runnable, Configurable {
     /** ERROR HANDLING API FRAMEWORK **/
 
     /**
-     * @param code
-     * @overload
+     * Responds to the given Request with the specified HTTP Error Response using the method
+     * proffered by the user via configuration (Static File Response, Plugin Response, Redirection,
+     * execute url, ect.)
+     *
+     * @param r    The Request to respond to
+     * @param code The HTTP Error Code
      */
     public void triggerHTTPError(Request r, int code) {
         triggerHTTPError(r, (double) code, (String) null);
     }
 
     /**
-     * @param code
-     * @param message
-     * @overload
+     * Responds to the given Request with the specified HTTP Error Response using the method
+     * proffered by the user via configuration (Static File Response, Plugin Response, Redirection,
+     * execute url, ect.)
+     *
+     * @param r       The Request to respond to
+     * @param code    The HTTP Error Code
+     * @param message The HTTP Error Message
      */
     public void triggerHTTPError(Request r, int code, String message) {
         triggerHTTPError(r, (double) code, message);
     }
 
     /**
-     * @param code
-     * @param ex
-     * @overload
+     * Responds to the given Request with the specified HTTP Error Response using the method
+     * proffered by the user via configuration (Static File Response, Plugin Response, Redirection,
+     * execute url, ect.)
+     *
+     * @param r    The Request to respond to
+     * @param code The HTTP Error Code
+     * @param e    The HTTPError Object
      */
-    public void triggerHTTPError(Request r, int code, Exception ex) {
-        triggerHTTPError(r, (double) code, ex);
+    public void triggerHTTPError(Request r, int code, HTTPError e) {
+        triggerHTTPError(r, (double) code, e);
     }
 
     /**
-     * @param code
-     * @overload
+     * Responds to the given Request with the specified HTTP Error Response using the method
+     * proffered by the user via configuration (Static File Response, Plugin Response, Redirection,
+     * execute url, ect.)
+     *
+     * @param r    The Request to respond to
+     * @param code The HTTP Error Code
      */
     public void triggerHTTPError(Request r, double code) {
         triggerHTTPError(r, code, (String) null);
     }
 
     /**
-     * @param code
-     * @param message
-     * @overload
+     * Responds to the given Request with the specified HTTP Error Response using the method
+     * proffered by the user via configuration (Static File Response, Plugin Response, Redirection,
+     * execute url, ect.)
+     *
+     * @param r       The Request to respond to
+     * @param code    The HTTP Error Code
+     * @param message The HTTP Error Message
      */
     public void triggerHTTPError(Request r, double code, String message) {
         triggerHTTPError(r, code, message, null);
     }
 
     /**
-     * API for HTTP Errors.
+     * Responds to the given Request with the specified HTTP Error Response using the method
+     * proffered by the user via configuration (Static File Response, Plugin Response, Redirection,
+     * execute url, ect.)
      *
-     * @param code
-     * @param e
-     * @overload
+     * @param r    The Request to respond to
+     * @param code The HTTP Error Code
+     * @param e    The HTTPError Object
      */
     public void triggerHTTPError(Request r, double code, Exception e) {
         triggerHTTPError(r, code, null, e);
     }
 
     /**
-     * API for HTTP Errors. [Overload Master]
+     * Responds to the given Request with the specified HTTP Error Response using the method
+     * proffered by the user via configuration (Static File Response, Plugin Response, Redirection,
+     * execute url, ect.)
      *
-     * @param code
-     * @param message
-     * @param e
-     * @overload master
+     * @param r       The Request to respond to
+     * @param code    The HTTP Error Code
+     * @param message The HTTP Error Message
+     * @param e       The HTTPError Object
      */
     public void triggerHTTPError(Request r, double code, String message, Exception e) {
         Plugins.do_action("http_error_" + code, new Object[]{this, message, e});
@@ -193,10 +303,17 @@ public abstract class Site implements Runnable, Configurable {
 
     }
 
+    /**
+     * Configures the site
+     *
+     * @param c The Configuration
+     * @return Whether the configuration was successfully applied
+     */
     @Override
     public boolean configure(Configuration c) {
         return true;
     }
+
 
     /**
      * Utility Method: Determines if the Site is stopped
