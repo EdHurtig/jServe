1f0408a741b06cbdcd04ce24097e26abf0d8e7f4
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-11-16 00:38:55 -0500

    Documenting Configurable

--- src/jServe/Core/Configuration/Configurable.java
+++ src/jServe/Core/Configuration/Configurable.java
@@ -1,5 +1,15 @@
 package jServe.Core.Configuration;
 
+/**
+ * An Interface for defining classes that can be passed a configuration setup
+ * and use that to automatically configure themselves
+ */
 public interface Configurable {
+    /**
+     * Configures the instance
+     *
+     * @param c The Configuration
+     * @return Whether the configuration given was successfully applied
+     */
     public boolean configure(Configuration c);
 }
